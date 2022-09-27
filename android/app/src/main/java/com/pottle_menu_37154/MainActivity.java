289806e54e563d3565a56e062287d6596c0f5062
Crowdbotics <team@crowdbotics.com>
2022-09-27 00:03:37 +0000

    Initial commit.

---
+++ android/app/src/main/java/com/pottle_menu_37154/MainActivity.java
@@ -0,0 +1,15 @@
+package com.pottle_menu_37154;
+
+import com.facebook.react.ReactActivity;
+
+public class MainActivity extends ReactActivity {
+
+  /**
+   * Returns the name of the main component registered from JavaScript. This is used to schedule
+   * rendering of the component.
+   */
+  @Override
+  protected String getMainComponentName() {
+    return "pottle_menu_37154";
+  }
+}
---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native";
+
+export const globalOptions = {
+  name: "pottle_menu_37154",
+  url: "https://pottle_menu_37154.botics.co",
+  api: "https://pottle_menu_37154.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    "copy": "Routes available!"
+  }
+}
